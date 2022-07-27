56d335ba3cfd80cbe037372d7bc903bbbdfaf8dd
Nicollas Ryan <nbarretoduarte@gmail.com>
2022-07-27 17:28:51 -0300

    Saving form data

--- src/Componets/NewTransactionModal/index.tsx
+++ src/Componets/NewTransactionModal/index.tsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal'
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import incomeImg from '../../assets/Entradas.svg'
 import outcomeImg from '../../assets/Saídas.svg'
 import closeImg from '../../assets/close.svg';
@@ -11,7 +11,15 @@ interface NewTrasactionModalProps {
 }
 
 export function NewTrasactionModal ({ isOpen, onRequestClose }: NewTrasactionModalProps) {
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit')
+
+    function handleCreateNewTransaction(event: FormEvent) {
+        event.preventDefault();
+        console.log({title, value, category, type})
+    }
 
 
     return (
@@ -30,16 +38,20 @@ export function NewTrasactionModal ({ isOpen, onRequestClose }: NewTrasactionMod
                 <img src={closeImg} alt="Fechar modal" />
             </button>
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
 
                 <input
                  placeholder='Título' 
+                 value={title}
+                 onChange={event => setTitle(event.target.value)}
                 />
 
                 <input 
                  type='number' 
                  placeholder='valor'
+                 value={value}
+                 onChange={event => setValue(Number(event.target.value))}
                  />
 
                 <TransactionTypeConatainer>
@@ -66,6 +78,8 @@ export function NewTrasactionModal ({ isOpen, onRequestClose }: NewTrasactionMod
 
                 <input 
                  placeholder='Categoria' 
+                 value={category}
+                 onChange={event => setCategory(event.target.value)}
                 />
 
                 <button type='submit'>
